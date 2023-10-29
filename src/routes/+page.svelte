1e8047d44f0387dc6ef9752288e68f8e32d44b7a
carloshatus <carlos.hatus@hotmail.com>
2023-10-29 16:01:05 -0300

    fix: print only one page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,70 @@
-<script>
-  let files;
-  let allItems;
-  let pages = [];
+<script lang="ts">
+  type ItemDetail = {
+    desc?: string;
+    obs?: string;
+    value: number;
+  };
+
+  type Item = {
+    name: string;
+    details: ItemDetail[];
+    desc?: string;
+  };
+
+  type Page = {
+    print?: boolean;
+    items: Item[];
+  };
+
+  let files: FileList;
+  let allItems: Item[];
+  let pages: Page[] = [];
   let maxPerPage = 6;
 
-  function sliceIntoChunks(arr, chunkSize) {
+  function sliceIntoChunks(arr: Item[], chunkSize: number): Page[] {
     const res = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
       const chunk = arr.slice(i, i + chunkSize);
-      res.push(chunk);
+      res.push({ items: chunk });
     }
     return res;
   }
 
-  function setItems(items) {
+  function setItems(items: Item[]): void {
     allItems = items;
     pages = sliceIntoChunks(items, maxPerPage);
   }
 
-  function setMaxItemsPerPage() {
+  function setMaxItemsPerPage(): void {
     console.log(maxPerPage);
     pages = sliceIntoChunks(allItems, maxPerPage);
   }
 
-  function print() {
-    pages.forEach((page, index) => {
+  function print(): void {
+    window.print();
+  }
+
+  function printPage(index: number): void {
+    pages = [
+      ...pages.map((page, i) => {
+        if (i !== index) {
+          page.print = false;
+        } else {
+          page.print = true;
+        }
+        return page;
+      }),
+    ];
+    setTimeout(() => {
       window.print();
-    });
+    }, 500);
   }
 
   $: if (files) {
     const read = new FileReader();
     read.readAsText(files[0]);
     read.onloadend = () => {
-      setItems(JSON.parse(read.result));
+      setItems(JSON.parse(String(read?.result) || ""));
     };
   }
 </script>
@@ -54,11 +85,16 @@
     max="6"
     on:change={setMaxItemsPerPage}
   />
-  <button on:click={print}>Imprimir</button>
 </div>
 {#each pages as page, i}
-  <div class="container" id={`page${i + 1}`}>
-    {#each page as item, i}
+  <div class="noprint">
+    <button on:click={() => printPage(i)}>{`Imprimir página ${i + 1}`}</button>
+  </div>
+  <div
+    class={`container page ${page.print ? "" : "noprint"}`}
+    id={`page${i + 1}`}
+  >
+    {#each page.items as item, i}
       <div class="card">
         <p>{item.name}</p>
         <div>
@@ -118,9 +154,16 @@
   .detail {
     padding-bottom: 15px;
   }
+  .page {
+    background-color: #fff;
+    border: 1px solid;
+  }
   @media print {
     .noprint {
       display: none;
+    }
+    .page {
+      border: none;
     }
     .container {
       break-after: always;
